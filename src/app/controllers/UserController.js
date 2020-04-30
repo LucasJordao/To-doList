7e1f11f89303ef30804a92bfe6c93edef6301278
Lucas William <59783795+LucasJordao@users.noreply.github.com>
2020-04-30 19:17:40 -0300

    Criando o store do UserController

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,36 @@
+const User = require('../models/User');
+
+class UserController{
+
+// Store - método responsável por criar novos Usuários (Provider ou Employee)
+  async store(req, res){
+
+    const { email } = req.body;
+
+// Verificar se já existe usuário com esse email
+
+    const checkEmail = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if(checkEmail){
+      return res.status(400).json({error: "User already exists!"});
+    };
+
+// Caso a verificação passe, criamos o usuário
+
+    const { name, password_hash, provider } = await User.create(req.body);
+
+    return res.json({
+      name,
+      email,
+      password_hash,
+      provider
+    })
+
+  }
+}
+
+module.exports = new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,16 @@
 const { Router } = require('express');
-const User = require('./app/models/User');
+
+// Aqui fica todas as importações dos controllers
+const UserController = require('./app/controllers/UserController');
 
 // Criamos a variável routes armazenando a função Router do express que serve para definirmos rotas
-
 const routes = Router();
+
 
 // Aqui ficará todas as rotas definida
 
-routes.get('/', async (req, res) => {
-  await User.create({
-    name: 'Lucas William Silva Jordão',
-    email: 'çls@hotmail.com',
-    password_hash: 'docker',
-    provider: true
-  });
-});
+// Rotas POST sem JWT
+routes.post('/users', UserController.store);
+
 
 module.exports = routes;
